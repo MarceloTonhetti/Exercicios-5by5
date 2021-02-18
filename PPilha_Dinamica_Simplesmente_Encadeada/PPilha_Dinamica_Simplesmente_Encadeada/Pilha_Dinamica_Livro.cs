4560ec060c3f0b479087b69ec84fb069eaa221dc
MarceloTonhetti <marcelotjunior2507@gmail.com>
2021-02-18 08:52:47 -0300

    Subindo projeto da Pilha Dinamica Simplesmente Encadeada

---
+++ PPilha_Dinamica_Simplesmente_Encadeada/PPilha_Dinamica_Simplesmente_Encadeada/Pilha_Dinamica_Livro.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPilha_Dinamica_Simplesmente_Encadeada
+{
+    class Pilha_Dinamica_Livro
+    {
+        public Livro Topo { get; set; }
+
+        public void Push(Livro aux)
+        {
+            //if (vazia()) //Verifica se a pilha está vazia
+            //{
+            //    Topo = aux;
+            //}
+            //else
+            //{
+                aux.Anterior = Topo;
+                Topo = aux;
+            //}
+            Console.WriteLine("Elemento inserido com sucesso!");
+            return;
+        }
+
+        private bool Vazia()
+        {
+            if (Topo == null)
+                return true;
+            return false;
+        }
+
+        public void Imprimir()
+        {
+            if (Vazia())
+            {
+                Console.WriteLine("Impossível Imprimir! Pilha Vazia!");
+            }
+            else
+            {
+                Livro aux = Topo;
+                Console.WriteLine("Os elementos na pilha são:");
+                do
+                {
+                    Console.WriteLine(aux.ToString());
+                    aux = aux.Anterior;
+                } while (aux != null);
+                Console.WriteLine(">>>FIM<<");
+            }
+        }
+
+        public void Pop()
+        {
+            if (Vazia())
+            {
+                Console.WriteLine("Impossível Remover! Pilha Vazia!");
+            }
+            else
+            {
+                Topo = Topo.Anterior;
+                Console.WriteLine("Elemento Removido!");
+            }
+        }
+
+        public int QtdElementos()
+        {
+            if (Vazia())
+            {
+                Console.WriteLine("Impossível contar! Pilha Vazia!");
+                return 0;
+            }
+            else
+            {
+                Livro aux = Topo;
+                int count=1;
+
+                while (aux.Anterior != null)
+                {
+                    count++;
+                    aux = aux.Anterior;
+                }
+
+                return count;
+            }
+        }
+
+        public void FindByTitle()
+        {
+            if (Vazia())
+                Console.WriteLine("Impossível realizar busca! Pilha Vazia!");
+            else
+            {
+                Livro aux = Topo;
+                int count=1;
+                string nomeLivro;
+
+                Console.Write("Informe o nome do livro que deseja procurar: ");
+                nomeLivro = Console.ReadLine();
+
+                do
+                {
+                    if(aux.Titulo == nomeLivro)
+                    {
+                        Console.WriteLine("Achei o livro que voce buscou: ");
+                        Console.WriteLine(aux.ToString());
+                        return;
+                    }
+                    else
+                        aux = aux.Anterior;
+                }while (aux.Anterior != null);
+
+                Console.WriteLine("Não foi encontrado nenhum livro com esse titulo!!!");
+                return;
+            }
+        }
+
+    }
+}
---
+++ PPilha_Dinamica_Simplesmente_Encadeada/PPilha_Dinamica_Simplesmente_Encadeada/Program.cs
@@ -0,0 +1,119 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPilha_Dinamica_Simplesmente_Encadeada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                1-inserir - ok
+                2-remover - ok
+                3-imprimir - ok
+                4-retornar quantidade de livros - ok
+                5-Buscar livros pelo nome
+                0-sair - ok
+
+                -contar quantos livros tem (retornar a quantidade de livros da pilha)
+                -Busca pelo titulo do livro 
+            */
+
+            int opMenu;
+
+            Pilha_Dinamica_Livro minha_pilha = new Pilha_Dinamica_Livro { Topo = null };
+
+            do
+            {
+                opMenu = Menu();
+                switch (opMenu)
+	            {
+		            case 1:
+                        InserirLivro(minha_pilha);
+                        break;
+                    case 2:
+                        RemoverLivro(minha_pilha);
+                        break;
+                    case 3:
+                        ImprimirPilha(minha_pilha);
+                        break;
+                    case 4:
+                        QtdLivros(minha_pilha);
+                        break;
+                    case 5:
+                        BuscarLivroPorTitulo(minha_pilha);
+                        break;
+	            }
+            }while(opMenu != 0);
+
+
+
+          /*
+           * minha_pilha.Imprimir();
+            Console.ReadKey();
+
+
+            minha_pilha.Imprimir();
+
+            minha_pilha.pop();
+            minha_pilha.Imprimir();
+            minha_pilha.pop();
+            minha_pilha.Imprimir();
+            minha_pilha.pop();*/
+
+            Console.ReadKey();
+        }
+
+        static int Menu()
+        {
+            int op;
+
+            Console.WriteLine(">>>>>>>>>>>   Menu   <<<<<<<<<<<\n");
+            Console.WriteLine("1 - Inserir livro");
+            Console.WriteLine("2 - Remover livro");
+            Console.WriteLine("3 - Imprimir pilha de livros");
+            Console.WriteLine("4 - Quantidade de elementos na pilha");
+            Console.WriteLine("5 - Buscar livro na pilha por título");
+            Console.WriteLine("0 - Sair");
+
+            if(int.TryParse(Console.ReadLine(), out op))
+                return op;
+
+            Console.WriteLine("Você nao digitou um numero.");
+            return -1;
+        }
+
+        static void InserirLivro(Pilha_Dinamica_Livro minha_pilha)
+        {
+            string t = Console.ReadLine();
+            string a = Console.ReadLine();
+            int i = int.Parse(Console.ReadLine());
+            
+            minha_pilha.Push(new Livro { Titulo = t, Autor = a, Isbn = i });
+        }
+
+        static void RemoverLivro(Pilha_Dinamica_Livro minha_pilha)
+        {
+            minha_pilha.Pop();
+        }
+
+        static void ImprimirPilha(Pilha_Dinamica_Livro minha_pilha)
+        {
+            minha_pilha.Imprimir();
+        }
+
+        static void QtdLivros(Pilha_Dinamica_Livro minha_pilha)
+        {
+            Console.WriteLine("Existem {0} livros empilhados!!!", minha_pilha.QtdElementos());
+        }
+
+        static void BuscarLivroPorTitulo(Pilha_Dinamica_Livro minha_pilha)
+        {
+            minha_pilha.FindByTitle();
+        }
+    }
+}
