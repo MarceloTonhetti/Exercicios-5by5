4c796438150f94e72d517ca7365f0a6e8a31e683
MarceloTonhetti <marcelotjunior2507@gmail.com>
2021-02-18 00:04:05 -0300

    Implementando o menu

--- excContaBancaria/excContaBancaria/Program.cs
+++ excContaBancaria/excContaBancaria/Program.cs
@@ -10,6 +10,37 @@ namespace excContaBancaria
 	{
 		static void Main(string[] args)
 		{
+			int op;
+
+			do
+			{
+				op = Menu();
+
+				switch (op)
+				{
+					case 1:
+						break;
+				}
+			} while (op != 0);
 		}
+
+		static int Menu()
+		{
+			int op;
+
+			Console.WriteLine("1 - Cadastrar dados do Cliente e da Conta.");
+			Console.WriteLine("2 - Realizar Depositósito.");
+			Console.WriteLine("3 - Realizar Saque");
+			Console.WriteLine("4 - Realzar Transferência");
+			Console.WriteLine("5 - Imprimir Saldo");
+			Console.WriteLine("0 - Sair do Sistema");
+
+			Console.Write("Escolha a opção do menu: ");
+			op = int.Parse(Console.ReadLine());
+
+			return op;
+		}
+
+
 	}
 }
